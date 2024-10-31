6cb72b80b13a3072abfda46de4454f4db94fe2a3
Débora Vieira <deboraluizafono@gmail.com>
2024-10-31 16:47:21 +0000

    update

--- restEspecialidade.py
+++ restEspecialidade.py
@@ -3,7 +3,7 @@ from restaurantes import *
 class Hamburgueria(Restaurante):
     def __init__(self, nome, localizacao, status, tservico,tmolho):
         super().__init__(nome, localizacao, status, tservico) #chama o construtor da classe mãe
-        self.molho = tmolho #cheddar,verde,barbecue,mostarda,caeser,kebab,maionese,ketchup, coctail,picante
+        self.tmolho = tmolho #cheddar,verde,barbecue,mostarda,caeser,kebab,maionese,ketchup, coctail,picante
 
      #definição de metodo
     def mostra_restaurante(self):
@@ -12,16 +12,25 @@ class Hamburgueria(Restaurante):
               Localização: {self.localizacao}
               Status: {self.status}
               Tipo de Serviço: {self.tservico}
-              Molho: {self.molho}
+              Molho: {self.tmolho}
               Pagamento: {self.pagamento}
               ''')
+     
+     
+     #metodo para escolher molho   
+    def escolheMolho(self,tmolho):
+        print(f'O molho escolhido foi {tmolho}')
+
+   
+###################################################################################################################################################
     
 class Italiano(Restaurante):
-    def __init__(self, nome, localizacao, status, tservico, tmassa):
+    def __init__(self, nome, localizacao, status, tservico, tmassa,tpizza):
         super().__init__(nome, localizacao, status, tservico) #chama o construtor da classe mãe
-        self.massa = tmassa #espagetti,talharim,gnocchi,ravioli,tortelini,fusilli,penne,lasanha,fetuccini
+        self.tmassa = tmassa #espagetti,talharim,gnocchi,ravioli,tortelini,fusilli,penne,lasanha,fetuccini
+        self.tpizza = tpizza #quatro queijos,calabresa,frango com catupiry, peperoni, romana, havaiana, portuguesa
     
-    #definição de metodo
+    #definição de metodo (herdado classe mãe - sobreposto)
     def mostra_restaurante(self):
         print(f''' Características do restaurante:
               Nome: {self.nome}
@@ -29,15 +38,28 @@ class Italiano(Restaurante):
               Status: {self.status}
               Tipo de Serviço: {self.tservico}
               Massa: {self.massa}
+              Pizza: {self.pizza}
               Pagamento: {self.pagamento}
-              ''')        
+              ''')   
+    
+    #metodo para escolher massa
+    def escolheMassa(self,tmassa):
+        print(f'A massa escolhida foi {tmassa}')
+
             
+    #metodo para escolher pizza
+    def escolhePizza(self,tpizza):
+        print(f'A pizza escolhida foi {tpizza}')
+    
+     
+        
+ ###################################################################################################################################################           
 class Asiatico(Restaurante):
     def __init__(self, nome, localizacao, status, tservico, trodizio):
         super().__init__(nome, localizacao, status, tservico) #chama o construtor da classe mãe
-        self.trodizio = trodizio #sushi, sashimi,spring roll,pratos chineses,curry tailandês,noodles,temaki
+        self.trodizio = trodizio #simples,premium,tradicional,supreme
         
-     #definição de metodo
+     #definição de metodo (herdado classe mãe - sobreposto)
     def mostra_restaurante(self):
         print(f''' Características do restaurante:
               Nome: {self.nome}
@@ -47,13 +69,19 @@ class Asiatico(Restaurante):
               Rodízio: {self.rodizio}
               Pagamento: {self.pagamento}
               ''')     
+        
+     #metodo para escolher o tipo de rodízio
+    def escolheRodizio(self,trodizio):
+        print(f'O rodízio escolhido foi {trodizio}')
 
+
+###################################################################################################################################################
 class Pastelaria(Restaurante):
     def __init__(self, nome, localizacao, status, tservico, tdoce):
         super().__init__(nome, localizacao, status, tservico) #chama o construtor da classe mãe
-        self.tdoce = tdoce  #bolo,pastel de nata, docinho, miniaturas,eclair,bolachas,tartes,quindim
+        self.tdoce = tdoce  #bolos,pastel de nata, docinho, miniaturas,eclair,bolachas,tartes,quindim
         
-     #definição de metodo
+     #definição de metodo (herdado classe mãe - sobreposto)
     def mostra_restaurante(self):
         print(f''' Características do restaurante:
               Nome: {self.nome}
@@ -62,14 +90,19 @@ class Pastelaria(Restaurante):
               Tipo de Serviço: {self.tservico}
               Doçaria: {self.tdoce}
               Pagamento: {self.pagamento}
-              ''')       
- 
-class Saudavel(Restaurante):
-    def __init__(self, nome, localizacao, status, tservico, tsumo):
-        super().__init__(nome, localizacao, status, tservico) #chama o construtor da classe mãe
-        self.tsumo = tsumo  #sumo verde, sumo da imunidade, sumo energizante, sumo antioxidante, sumo detox, sumo refrescante
+              ''')     
+          
+     #metodo para escolher o doce
+    def escolheDoce(self,tdoce):
+        print(f'O doce escolhido foi {tdoce}')
         
-     #definição de metodo
+################################################################################################################################################### 
+class Saudavel(Restaurante):
+    def __init__(self, nome, localizacao, status, tservico, tsmothie):
+        super().__init__(nome, localizacao, status, tservico) #chama o construtor da classe mãe
+        self.tsmothie = tsmothie  #smothie verde, smothie da imunidade, smothie energizante, smothie antioxidante, smothie detox, smothie refrescante
+        
+     #definição de metodo (herdado classe mãe - sobreposto)
     def mostra_restaurante(self):
         print(f''' Características do restaurante:
               Nome: {self.nome}
@@ -78,4 +111,56 @@ class Saudavel(Restaurante):
               Tipo de Serviço: {self.tservico}
               Tipo de Sumo: {self.tsumo}
               Pagamento: {self.pagamento}
-              ''')         
+              ''')  
+     
+     #metodo para mostrar o smothie
+    def mostraSmothie(self,tsmothie):
+        print(f'O smothie escolhido foi {tsmothie}')
+
+###################################################################################################################################################
+class LusoBrasileiro(Restaurante):
+    def __init__(self, nome, localizacao, status, tservico, tculinaria,opcoes):
+        super().__init__(nome, localizacao, status, tservico) #chama o construtor da classe mãe
+        self.tculinaria = tculinaria  #portuguesa,brasileira
+        self.opcoes = opcoes  #menu do dia, sugestão do chef, a la carte
+        
+     #definição de metodo (herdado classe mãe - sobreposto)
+    def mostra_restaurante(self):
+        print(f''' Características do restaurante:
+              Nome: {self.nome}
+              Localização: {self.localizacao}
+              Status: {self.status}
+              Tipo de Serviço: {self.tservico}
+              Tipo de Culinária: {self.tculinaria}
+              Opções de Menu: {self.opcoes}
+              Pagamento: {self.pagamento}
+              ''')     
+    
+     #metodo para escolher o tipo de culinaria
+    def escolheCulinaria(self):
+        if self.tculinaria == 'brasileira':
+            opcao_escolhida = (input('''Selecione uma opção: mineira, churrasco, nordestina, capixaba'''))
+            if opcao_escolhida == 'mineira':
+                print(f'Você escolheu a culinária  brasileira e o tipo de comida {opcao_escolhida}')
+            elif opcao_escolhida == 'churrasco':
+                print(f'Você escolheu a culinária  brasileira e o tipo de comida {opcao_escolhida}')
+            elif opcao_escolhida == 'nordestina':
+                print(f'Você escolheu a culinária  brasileira e o tipo de comida {opcao_escolhida}')
+            elif opcao_escolhida == 'capixaba':
+                 print(f'Você escolheu a culinária  brasileira e o tipo de comida {opcao_escolhida}')
+            else:
+                print(f'Infelizmente não dispomos desse tipo de comida ainda.Tente novamente!')
+        elif self.tculinaria == 'portuguesa':
+            opcao_escolhida = (input('''Selecione uma opção: alentejana, transmontana, minhota, ribatejo'''))
+            if opcao_escolhida == 'alentejana':
+                 print(f'Você escolheu a culinária  portuguesa e o tipo de comida {opcao_escolhida}')
+            elif opcao_escolhida == 'transmontana':
+                 print(f'Você escolheu a culinária  portuguesa e o tipo de comida {opcao_escolhida}')
+            elif opcao_escolhida == 'minhota':
+                 print(f'Você escolheu a culinária  portuguesa e o tipo de comida {opcao_escolhida}')
+            elif opcao_escolhida == 'ribatejo':
+                 print(f'Você escolheu a culinária  portuguesa e o tipo de comida {opcao_escolhida}')
+            else:
+                print(f'Infelizmente não dispomos desse tipo de comida ainda.Tente novamente!')  
+        else:
+            print(f'Infelizmente não dispomos dessa culinária.Tente novamente!')        
--- restaurantes.py
+++ restaurantes.py
@@ -1,12 +1,14 @@
 #criação da classe restaurantes
 class Restaurante:
+    contadorRestaurante = 0             #contador de restaurantes
     def __init__(self, nome,localizacao,status,tservico):
         self.pagamento = 'Cartão'      #atributo fixo, não passado como argumento
         self.nome = nome
         self.localizacao = localizacao  #bairros de lisboa
         self.status = status            #aberto/fechado
         self.tservico = tservico         #delivery,takeaway,comer no local
-    
+        Restaurante.contadorRestaurante += 1
+        
     #definição dos métodos    
     def mostra_restaurante(self):
         print(f''' Características do restaurante:
@@ -16,6 +18,12 @@ class Restaurante:
               Tipo de Serviço: {self.tservico}
               Pagamento: {self.pagamento}
               ''')
-    
         
+    #função para mostrar o contador
+    def mostra_contador():
+        print(f'Número de restaurantes: {Restaurante.contadorRestaurante}')
     
+    #função para mostrar o modo de pagamento
+    def mostra_pagamento(self,pagamento):
+        self.pagamento = pagamento
+        print(f'O pagamento escolhido foi {self.pagamento}')
